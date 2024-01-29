427a7a65d893ae9b088ab1894f61bd242e7168e7
Nicholas McKee <nicholas.mckee@mail.utoronto.ca>
2024-01-29 13:57:26 -0500

    Added fallback for non-encoded characters

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ import csv
 import reportlab
 from PyPDF2 import PdfWriter, PdfReader
 import io
+from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+import glob
 
 cert = "template.pdf"
+fonts_dir = "Fonts" # Folder containing all fonts being
+Y = 217
 
-pdfmetrics.registerFont(TTFont("Inter", "Inter-Regular.ttf"))
+# Set default font here
+fonts = [("Inter", "Fonts/Inter-Regular.ttf")]
+
+# Fallback fonts for characters not encoded in default
+fonts.extend([(name[:-3], name) for name in glob.glob(f"{fonts_dir}/*.ttf")])
+
+font_widths = {}
+
+for font_name, file in fonts:
+    font = TTFont(font_name, file)
+    pdfmetrics.registerFont(font)
+    font_widths[font_name] = font.face.charWidths
 
 
 def create_certificates(names: list):
@@ -21,8 +36,40 @@ def create_certificates(names: list):
         page = template.pages[0]
         c = reportlab.pdfgen.canvas.Canvas(packet, pagesize=(
             page.mediabox.width, page.mediabox.height))
+        page_width = int(page.mediabox.width)
         c.setFont("Inter", 40)
-        c.drawCentredString(420, 217, name)
+
+        if "(" in name:
+            alt_name = name[name.index("(") + 1:name.index(")")]
+            ascii_name = name[:name.index("(") - 1]
+
+            used_font = ""
+            for f_name, width in font_widths.items():
+                if all(ord(char) in width for char in alt_name):
+                    used_font = f_name
+                    break
+
+            if used_font == "":
+                print(f"Unable to find font for: {alt_name}")
+                continue
+
+            alt_width = stringWidth(alt_name, used_font, 40)
+            full_offset = stringWidth(ascii_name + " ()", "Inter",
+                                      40) + alt_width
+            ascii_offset = stringWidth(ascii_name + " (", "Inter",
+                                       40) - alt_width - 7
+            end_offset = full_offset - stringWidth(")", "Inter",
+                                                   40)
+
+            c.drawString((page_width - full_offset) / 2, Y, ascii_name + " (")
+            c.drawString((page_width + end_offset) / 2, Y, ")")
+
+            c.setFont(used_font, 40)
+            c.drawString((page_width + ascii_offset) / 2, Y, alt_name)
+
+        else:
+            c.drawCentredString(page_width / 2, Y, name)
+
         c.save()
 
         packet.seek(0)
@@ -46,11 +93,13 @@ if __name__ == "__main__":
 
     persons = []
 
-    with open(args.filename) as reg_file:
+    with open(args.filename, encoding='utf8') as reg_file:
         next(reg_file)
         reg = csv.reader(reg_file)
         for person in reg:
             if person[3] == "" or (person[3][:4] == "2024" and args.all):
                 persons.append(person[1])
+
+    persons.sort()
 
     create_certificates(persons)
